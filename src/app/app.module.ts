ff83a29a9dc66bc780a71760d5d34e291b98befb
Libertual <peterworld@gmail.com>
2017-03-25 13:02:34 +0100

    Api client point to api server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule }  from '@angular/platform-browser';
+import { NgModule }       from '@angular/core';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule }     from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent }   from './app.component';
+import { Routing }        from './app.routing';
+
+import { AuthGuard } from './_guards/index';
+import { AuthenticationService, UserService } from './_services/index';
+
+import { LoginComponent } from './login/index';
+import { HomeComponent } from './home/index';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Routing
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AuthenticationService,
+    UserService,
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,15 @@
+import { RouterModule } from '@angular/router';
+
+import { LoginComponent }       from './login/index';
+import { HomeComponent }        from './home/index';
+import { AuthGuard }            from './_guards/index';
+
+const appRoutes = [
+    { path: 'login', component: LoginComponent },
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const Routing = RouterModule.forRoot(appRoutes);
